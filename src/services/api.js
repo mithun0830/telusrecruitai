42dab595522e2737053bc5e6e9562c5084e9bb9f
mithun <mithunwalawalkar100@gmail.com>
2025-07-18 11:50:13 +0530

    removed kong URL

--- src/services/api.js
+++ src/services/api.js
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
-const API_BASE_URL = 'https://kong-dp-865090871947.asia-south1.run.app/api';
-const NOTIFICATION_BASE_URL = 'https://kong-dp-865090871947.asia-south1.run.app/api';
-const AI_SEARCH_BASE_URL = 'https://kong-dp-865090871947.asia-south1.run.app/api';
+const API_BASE_URL = 'https://recruitai-authentication-865090871947.asia-south1.run.app/api';
+const NOTIFICATION_BASE_URL = 'https://notification-service-865090871947.asia-south1.run.app/api';
+const AI_SEARCH_BASE_URL = 'https://aimatch-lock-865090871947.asia-south1.run.app/api';
 
 // Create axios instances with default config
 const api = axios.create({
